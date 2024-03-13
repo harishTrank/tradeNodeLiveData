9663c9fdf9a9bf9b65b3fa168368df30a2328ab5
harish1907 <nehasharma@tranktechies.com>
2024-03-13 15:36:41 +0530

    update

--- Services/pendingOrderManager.js
+++ Services/pendingOrderManager.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 const axios = require("axios");
 
-const host = "3.108.218.24";
+const host = "65.0.30.73";
 const endPoints = `${host}:8000`;
 const pool = new Pool({
   host,
